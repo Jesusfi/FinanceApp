ca9da1e1c4d14e859c46a5503061b7e8ce444d20
jesus <izquierdo843@gmail.com>
2019-04-01 23:25:18 -0400

    Started working with notifications

--- app/src/main/java/com/codegud/financeapp/DashBoardActivity.java
+++ app/src/main/java/com/codegud/financeapp/DashBoardActivity.java
@@ -1,6 +1,10 @@
 package com.codegud.financeapp;
 
 import android.app.ActivityOptions;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -11,6 +15,7 @@ import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
 import android.support.v4.view.ViewCompat;
@@ -117,6 +122,9 @@ public class DashBoardActivity extends AppCompatActivity implements AddCategoryL
                 AddEnvelopDialogFragment addEnvelopDialogFragment = new AddEnvelopDialogFragment();
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 addEnvelopDialogFragment.show(fragmentManager, "TAG");
+
+                //showNotification("Update Your Budget","This is a reminder to update your budget ");
+
             }
         });
 
@@ -250,5 +258,25 @@ public class DashBoardActivity extends AppCompatActivity implements AddCategoryL
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    void showNotification(String title, String content) {
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default",
+                    "YOUR_CHANNEL_NAME",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("YOUR_NOTIFICATION_CHANNEL_DISCRIPTION");
+            mNotificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
+                .setSmallIcon(R.drawable.logo) // notification icon
+                .setContentTitle(title) // title for notification
+                .setContentText(content)// message for notification
+                .setAutoCancel(true); // clear notification after click
+        Intent intent = new Intent(getApplicationContext(), DashBoardActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pi);
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
